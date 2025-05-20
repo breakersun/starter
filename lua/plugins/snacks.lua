e6f2c43e933ca07b15371cb9d615e50db6dc399d
breaker_ <leosunsl@outlook.com>
2025-05-20 21:33:45 +0800

    Create snacks.lua

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      enabled = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
+  },
+}
