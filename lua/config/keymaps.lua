4d31814ed1ad01bb863eb88e3df96af6d98d19f5
leosun <leosunsl@outlook.com>
2025-05-22 10:39:31 +0800

    feat: smart dd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- smart dd: when dd a empty line, then don't touch registers
+local smart_dd = function()
+  if vim.api.nvim_get_current_line():match("^%s*$") then
+    return '"_dd'
+  else
+    return "dd"
+  end
+end
+vim.keymap.set("n", "dd", smart_dd, { noremap = true, expr = true })
