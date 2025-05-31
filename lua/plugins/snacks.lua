d1f2a7ee4a23be444c19ccc90626fc0122b98324
leosun <leosunsl@outlook.com>
2025-05-31 23:38:58 +0800

    using D to diff select files in explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,6 +13,23 @@ return {
               position = "right",
             },
           },
+          actions = {
+            diff = {
+              action = function(picker)
+                picker:close()
+                local sel = picker:selected()
+                if #sel > 0 and sel then
+                  Snacks.notify.info(sel[1].file)
+                  vim.cmd("tabnew " .. sel[1].file)
+                  vim.cmd("vert diffs " .. sel[2].file)
+                  Snacks.notify.info("Diffing " .. sel[1].file .. " against " .. sel[2].file)
+                  return
+                end
+                Snacks.notify.info("Select two entries for the diff")
+              end,
+            },
+          },
+          win = { list = { keys = { ["D"] = "diff", }, }, },
         },
       },
     },
