be480d6133303e5e27b2244466df7d4165e29333
leosun <leosunsl@outlook.com>
2025-06-18 11:40:36 +0800

    update ufo to exclude some file types

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,6 +1,9 @@
 return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" },
+  opts = {
+    filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason', 'snacks_picker_list' },
+  },
   config = {
     close_fold_kinds = { "imports" },
   },
