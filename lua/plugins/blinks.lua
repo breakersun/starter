4d383f82a91ebfccc5d43fea46a570f52067bd9c
leosun <leosunsl@outlook.com>
2026-01-12 11:31:54 +0800

    fix super tab for snippets

--- lua/plugins/blinks.lua
+++ lua/plugins/blinks.lua
@@ -4,8 +4,15 @@ return {
     completion = { list = { selection = { preselect = false, auto_insert = true } } }, 
     keymap = {
       preset = "default",
-      ["<Tab>"] = { "select_next" },
-      ["<S-Tab>"] = { "select_prev" },
+      ['<Tab>'] = {
+        function(cmp)
+          if cmp.snippet_active() then return cmp.accept()
+          else return cmp.select_and_accept() end
+        end,
+        'snippet_forward',
+        'fallback'
+      },
+      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
     },
   },
 }
