482eae61b518b442be9b7c4cd8cb54fabd5add4e
leosun <leosunsl@outlook.com>
2026-01-16 11:01:17 +0800

    make substitue key-bindings work for open file

--- lua/plugins/substitue.lua
+++ lua/plugins/substitue.lua
@@ -1,6 +1,7 @@
 return {
   "gbprod/substitute.nvim",
-  event = "BufReadPost",
+  -- event = "BufReadPost",
+  event = "VeryLazy",
   config = function()
     require("substitute").setup({})
   end,
