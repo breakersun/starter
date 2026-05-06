9e3234a5dc0055f231b499a218a3dfd82687c77a
leosun <leosunsl@outlook.com>
2026-05-06 08:06:39 +0800

    moving lines use leader m j/k for better use glazewm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,13 @@ vim.keymap.set('n', '<Space>qQ', '<Cmd>qa<cr>', { desc = 'Quit All' })
 vim.keymap.set('n', '<Space>ch', '<Cmd>ClangdSwitchSourceHeader<cr>', { desc = 'Swap Header/Impl File'})
 
 vim.keymap.set('n', 'gp', '`[v`]')
+
+-- remove alt-j/k in case of glazewm or i3wm
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+vim.keymap.set("n", "<leader>mj", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<leader>mk", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<C-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<C-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<leader>mj", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<leader>mk", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
